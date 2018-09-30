6752e7e385dc0b0eee4dcb1499598bdf7f7aeec8
Mesut Piskin <mesutpiskin@outlook.com>
2018-09-30 16:28:12 +0300

    ble support

--- scan_ble_devices.py
+++ scan_ble_devices.py
@@ -1,10 +1,17 @@
 
 import blescan
 import sys
-
+from firebase import firebase
 import bluetooth._bluetooth as bluez
 
-dev_id = 0 #varsayilan bl modulu
+firebase = firebase.FirebaseApplication(
+    'https://beacon-5d432.firebaseio.com/', None)  # database
+customers = firebase.get('/', None)  # tum macler
+for k, v in customers.iteritems():
+    print k, v
+
+
+dev_id = 0  # varsayilan bl modulu
 try:
 	sock = bluez.hci_open_dev(dev_id)
 	print "ble thread started"
@@ -22,3 +29,10 @@ while True:
 	for beacon in returnedList:
 		print beacon
 
+	print "---------- ALAN ICERSINDE OLAN KISILER--------"
+	for beacon in returnedList:
+		for key, value in customers.iteritems():
+			if beacon.find(str(value) != -1:
+        		ad="Adi:" + str(key)
+				mac=" - Mac Adresi:" + str(value)
+				print ad, mac
