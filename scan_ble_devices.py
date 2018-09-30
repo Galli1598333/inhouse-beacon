f3041deaa6dc6515bac6d6ba1e482f34f9f06ee4
Mesut Piskin <mesutpiskin@outlook.com>
2018-09-30 18:35:25 +0300

    sqlite logger

--- scan_ble_devices.py
+++ scan_ble_devices.py
@@ -3,7 +3,30 @@ import blescan
 import sys
 from firebase import firebase
 import bluetooth._bluetooth as bluez
+import sqlite3
+import datetime
 
+
+#sqlite
+conn = sqlite3.connect('db/bl_log.db')
+
+
+def insert_log(bl_log_data):
+    sql = ''' INSERT INTO Log(CustomerName,MacAdress,InsertDate)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, bl_log_data)
+
+def insert_anon_log(bl_log_data):
+    sql = ''' INSERT INTO Log(Mac,InsertedDate)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, bl_log_data)
+
+
+
+
+#firebase
 firebase = firebase.FirebaseApplication(
     'https://beacon-5d432.firebaseio.com/', None)  # database
 customers = firebase.get('/', None)  # tum macler
@@ -11,6 +34,7 @@ for k, v in customers.iteritems():
     print k, v
 
 
+#bluetooth
 dev_id = 0  # varsayilan bl modulu
 try:
 	sock = bluez.hci_open_dev(dev_id)
@@ -24,15 +48,22 @@ blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
 while True:
+	now = datetime.datetime.now()
 	returnedList = blescan.parse_events(sock, 10)
-	print "----------SONUC--------"
+	print "----------BULUNAN CIHAZLAR--------"
 	for beacon in returnedList:
 		print beacon
+		bl_data = beacon.split(',')
+		mac = bl_data[0]
+		insert_anon_data=(mac,now.isoformat())
+		insert_anon_log(insert_anon_data)
 
-	print "---------- ALAN ICERSINDE OLAN KISILER--------"
+	print "----------TANINMLANAN KISILER--------"
 	for beacon in returnedList:
 		for key, value in customers.iteritems():
 			if beacon.find(str(value)) != -1:
         			ad = "Adi:" + str(key)
 				mac=" - Mac Adresi:" + str(value)
 				print ad, mac
+				insert_user_data=(str(key),str(value),now.isoformat())
+				insert_log(insert_user_data)
